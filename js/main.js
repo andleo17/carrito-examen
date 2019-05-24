4ab27158a0acaa651c8588584dec8450d8efed07
andleo17 <andres.gastulo@gmail.com>
2019-05-23 23:15:13 -0500

    terminé

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ var carrito = [];
 
 $(document).ready(function () {
     listarCursos();
+    mostrarCarrito();
 });
 
 function listarCursos() {
@@ -44,12 +45,86 @@ function mostrarCarrito() {
     if (carrito.length != 0) {
         carrito.forEach(producto => {
             p = '<tr>';
+            p +=    '<td class="text-center"><button class="btn btn-danger btn-sm" onclick="eliminarDeCarrito(' + producto[0] + ')">X</button></td>';
             p +=    '<td>' + producto[1] + '</td>';
             p +=    '<td> S/ ' + producto[2] + '</td>';
-            p +=    '<td><button class="btn btn-danger btn-sm">X</button></td>';
             p += '</tr>';
             $("#tblDatos").append(p);
         });
-        
     }
+    mostrarTotal();
+}
+
+function eliminarDeCarrito(id) {
+    carrito.splice(carrito.indexOf(listaCursos[id - 1]), 1);
+    document.getElementById(id.toString()).disabled = false;
+    document.getElementById(id.toString()).innerHTML = "Agregar al carrito";
+    mostrarCarrito();
+    mostrarTotal();
+}
+
+var total;
+
+function mostrarTotal() {
+    $("#total").html("");
+    total = 0;
+    if (carrito.length != 0){
+        carrito.forEach(p => total = total + p[2]);
+        
+        t = '<td colspan="2">Total a pagar:</td>';
+        t += '<td>S/' + total + '</td>';
+    
+        $("#total").append(t);
+    }
+}
+
+function limpiarCarrito() {
+    carrito.forEach(p => {
+        document.getElementById(p[0].toString()).disabled = false;
+        document.getElementById(p[0].toString()).innerHTML = "Agregar al carrito";
+    });
+    carrito = [];
+    mostrarCarrito();
+}
+
+function comprar() {
+    nombres = $("#txtNombres").val();
+    email = $("#txtEmail").val();
+
+    $("#contenido-modal").html("");
+    c = "<h6>Confirme sus datos:</h6>";
+    c += '<span class="ml-4">Nombres y Apellidos: </span>';
+    c += '<span class="font-weight-bold">' + nombres + "</span><br>";
+    c += '<span class="ml-4">Email: </span>';
+    c += '<span class="font-weight-bold">' + email + "</span>";
+    c += '<table class="table table-sm mt-4">';
+    c +=    '<thead>';
+    c +=        '<tr>';
+    c +=            '<th>Producto</th>';
+    c +=            '<th>Precio</th>';
+    c +=        '</tr>';
+    c +=    '</thead>';
+    c +=    '<tbody>';
+    carrito.forEach(p => {
+        c +=    '<tr>';
+        c +=        '<td>' + p[1] + '</td>';
+        c +=        '<td>S/ ' + p[2] + '</td>';
+        c +=    '</tr>';
+    });
+    c +=    '</tbody>';
+    c +=    '<tfoot class="font-weight-bold">';
+    c +=        '<tr>';
+    c +=            '<td>Total:</td>';
+    c +=            '<td>S/ ' + total + '</td>';
+    c +=        '</tr>'
+    c +=    '</tfoot>';
+    c += '</table>'
+
+    $("#contenido-modal").append(c);
+}
+
+function confirmarCompra() {
+    limpiarCarrito();
+    $("#txtNombres").val("");
+    $("#txtEmail").val("");
 }
